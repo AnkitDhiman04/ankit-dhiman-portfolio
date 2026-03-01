fb5a6ca2e80dc75c861ff763d0b2e186cb8be2fa
AnkitDhiman04 <dhimanankit291997@gmail.com>
2026-03-01 18:48:56 +0530

    fix bug

--- script.js
+++ script.js
@@ -39,6 +39,21 @@ function setMoonIcon() {
 
 
 
+// it is for active and de-active button
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function () {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
 
 
 
